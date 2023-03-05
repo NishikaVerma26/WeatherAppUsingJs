b54bdae6323ddb994f0246a1db139adfa5c949dc
Nishika Verma <125440375+NishikaVerma26@users.noreply.github.com>
2023-03-05 14:51:13 +0530

    Update weatherapp.js

--- weatherapp.js
+++ weatherapp.js
@@ -2,10 +2,14 @@ const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
 const form = document.querySelector("form")
 const search = document.querySelector("#search")
 const weather = document.querySelector("#weather")
-    // const API = `https://api.openweathermap.org/data/2.5/weather
+    // const API = `https://api.openweathermap.org/data/2.5/weather?
+    // q=${city}&appid=${API_KEY}&units=metric`
+    // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
 const getWeather = async(city) => {
+    weather.innerHTML = `<h2> Loading... <h2>`
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    const response = await fetch(url)
+    const response = await fetch(url);
+    const data = await response.json()
     return showWeather(data)
 }
 
